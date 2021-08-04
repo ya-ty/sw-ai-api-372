4d146ee7bd7f456bb2748cdf60217058521940a2
yanshityagi <yanshi.tyagi@gmail.com>
2021-08-04 17:14:07 +0530

    Activation of virtual env

--- connection-file.py
+++ connection-file.py
@@ -6,10 +6,22 @@ try:
         user="yan2",
         password="yaN21525",
         host="3.109.48.72",
-        port=3306,
-        database="employees"
+        port= 34768,
+        database="final0729d"
 
     )
 except mariadb.Error as e:
     print(f"Error connecting to MariaDB Platform: {e}")
     sys.exit(1)
+
+cur = conn.cursor()
+
+
+cur.execute( "SELECT * FROM sensorevents")
+
+a = cur.fetchall()
+
+for row in a:
+    print(row)
+    print("\n")
+
--- developer-notes.md
+++ developer-notes.md
@@ -4,3 +4,7 @@
 
 1. [Python Virtual Environment Commands)[https://uoa-eresearch.github.io/eresearch-cookbook/recipe/2014/11/26/python-virtual-env/]
 
+
+## Common Commands
+
+1. Activate virtual environment `env372\Scripts\activate`
